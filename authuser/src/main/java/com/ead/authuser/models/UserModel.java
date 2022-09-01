eb17addfb5caea854266830f64b55b4ff5d97a91
Rodrigo Rech <rodrigorech92@gmail.com>
2022-09-01 14:06:36 -0300

    Feat authuser: UserModel created

---
+++ authuser/src/main/java/com/ead/authuser/models/UserModel.java
@@ -0,0 +1,54 @@
+package com.ead.authuser.models;
+
+import com.ead.authuser.enums.UserStatus;
+import com.ead.authuser.enums.UserType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Table(name = "TB_USERS")
+public class UserModel implements Serializable {
+
+    private static final long serialVersionUID = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+    @JsonIgnore
+    @Column(nullable = false, length = 255)
+    private String password;
+    @Column(nullable = false, length = 150)
+    private String fullname;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+    @Column(length = 20)
+    private String phoneNumber;
+    @Column(length = 20)
+    private String cpf;
+    @Column
+    private String imageUrl;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(nullable = false)
+    private LocalDateTime creatingDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(nullable = false)
+    private LocalDateTime lastUpdateDate;
+
+}
