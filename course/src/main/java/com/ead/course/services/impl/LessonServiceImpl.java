c17168c951d157a1f6b76acf94d14e2d18b97b86
Rodrigo Rech <rodrigorech92@gmail.com>
2022-09-27 16:52:08 -0300

    Feat course: LessonService and LessonServiceImpl - methods findLessonIntoModule and delete implemented;

--- course/src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
@@ -6,6 +6,9 @@ import com.ead.course.services.LessonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class LessonServiceImpl implements LessonService {
 
@@ -14,5 +17,15 @@ public class LessonServiceImpl implements LessonService {
     @Override
     public LessonModel save(LessonModel lessonModel) {
         return lessonRepository.save(lessonModel);
+    }
+
+    @Override
+    public Optional<LessonModel> findLessonIntoModule(UUID moduleId, UUID lessonId) {
+        return lessonRepository.findLessonIntoModule(moduleId, lessonId);
+    }
+
+    @Override
+    public void delete(LessonModel lessonModel) {
+        lessonRepository.delete(lessonModel);
     }
 }
