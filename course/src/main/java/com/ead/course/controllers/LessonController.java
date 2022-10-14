95db3f6a350101cbc6b63c2528bcc557fab7b306
Rodrigo Rech <rodrigorech92@gmail.com>
2022-10-14 11:04:48 -0300

    Feat course: LessonController - getAllLessonsFromModule updated to return pageable with specification.

--- course/src/main/java/com/ead/course/controllers/LessonController.java
+++ course/src/main/java/com/ead/course/controllers/LessonController.java
@@ -5,8 +5,12 @@ import com.ead.course.models.LessonModel;
 import com.ead.course.models.ModuleModel;
 import com.ead.course.services.LessonService;
 import com.ead.course.services.ModuleService;
+import com.ead.course.specifications.SpecificationTemplate;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -73,12 +77,14 @@ public class LessonController {
     }
 
     @GetMapping("/modules/{moduleId}/lessons")
-    public ResponseEntity<Object> getAllLessonsFromAModule(@PathVariable(value = "moduleId") UUID moduleId) {
+    public ResponseEntity<Object> getAllLessonsFromAModule(@PathVariable(value = "moduleId") UUID moduleId,
+                                                            SpecificationTemplate.LessonSpec spec,
+                                                           @PageableDefault(page = 0, size = 10, sort = "lessonId", direction = Sort.Direction.ASC)Pageable pageable) {
         Optional<ModuleModel> moduleModelOptional = moduleService.findById(moduleId);
         if (!moduleModelOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module not found.");
         }
-        return ResponseEntity.status(HttpStatus.OK).body(lessonService.findAllLessonsIntoModule(moduleId));
+        return ResponseEntity.status(HttpStatus.OK).body(lessonService.findAllLessonsIntoModule(SpecificationTemplate.lessonModuleId(moduleId).and(spec), pageable));
     }
 
     @GetMapping("/modules/{moduleId}/lessons/{lessonId}")
